4e9fca02e38e10109f0dbe226fa3d1277726b47f
Rinku Gajbhiye <rinkugajbhiye1111@gmail.com>
2025-04-24 21:31:16 +0530

    subnet added

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
 provider "aws" {
-region ="us-east-1"
+  region = "us-west-2"
 
-tags{
+}
 
-  Name = "myaws"
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.10.0.0/16"
+  tags = {
+    Name = "MYVPC"
+  }
+
+}
+resource "aws_subnet" "Public" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.10.0.0/24"
+  tags = {
+    Name = "PublicSubnet"
+  }
 }
